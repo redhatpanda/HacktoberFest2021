83b202669661444b7a60e546467e139a630090b5
svadhishenoy95 <68854535+svadhishenoy95@users.noreply.github.com>
2021-10-31 19:04:03 +0530

    added

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 # Program make a simple calculator
-import math
+
 # This function adds two numbers
 def add(x, y):
     return x + y
@@ -16,23 +16,15 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-def squareroot(x):
-    return math.sqrt(x)
-
-
-print("Select operation.")
-print("1-Add")
-print("2-Subtract")
-print("3-Multiply")
-print("4-Divide")
-print("5-Square Root")
-print("6-sine")
-print("7-cosine")
-print("8-tangent")
+print("Select operation")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4/5/6/7/8): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
@@ -47,32 +39,13 @@ while True:
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
-
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
-            
-
+        
         # check if user wants another calculation
         # break the while loop if answer is no
-            next_calculation = input("Let's do next calculation? (yes/no): ")
+        next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
-    
-    elif(choice == '5'):
-        x = int(input("Enter the Number"))
-        print("square root of number is:",squareroot(x))
-    
-    elif(choice == '6'):
-        x = int(input("Enter your Number: "))
-        print(math.sin(x))
-    
-    elif(choice == '7'):
-        x = int(input("Enter your Number: "))
-        print(math.cos(x))
-
-    elif(choice == '8'):
-        x = int(input("Enter your Number: "))
-        print(math.tan(x))
-
     else:
-        print("invalid input")
+        print("Invalid Input")              
